1cead02f6bb546b60728ba265453fd9708a75e75
Shuangqi <shuangqi_li@epam.com>
2023-10-12 13:44:03 +0800

    feat(#4): add multiple line and ascii code supports

--- plugin/snippets.py
+++ plugin/snippets.py
@@ -7,14 +7,13 @@ Simple plugin to save key/value snippets and copy to clipboard.
 
 from flowlauncher import FlowLauncher, FlowLauncherAPI
 import sys
-import subprocess
+import ctypes
 import sqlite3
+import pyperclip
 
-db_name = "./snippets.db"
-
-def getValue(key):
+def getValue(dbName, key):
     value = {}
-    conn = sqlite3.connect(db_name)
+    conn = sqlite3.connect(dbName)
     cursor = conn.cursor()
 
     cursor.execute("SELECT value FROM snippets WHERE key=?", (key,))
@@ -26,23 +25,31 @@ def getValue(key):
     conn.close()
     return value
 
-def saveValue(key, value):
-    conn = sqlite3.connect(db_name)
+def saveValue(dbName, key, value):
+    conn = sqlite3.connect(dbName)
     cursor = conn.cursor()
     cursor.execute("INSERT OR REPLACE INTO snippets (key, value) VALUES (?, ?)", (key, value))
     conn.commit()
     conn.close()
-    cmd = 'echo '+ value +'|clip'
-    return subprocess.check_call(cmd, shell=True)
+    copy2clip(dbName, value)
 
-def copy2clip(txt):
+def deleteValue(dbName, key):
+    conn = sqlite3.connect(dbName)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM  snippets WHERE key=?", (key,))
+    conn.commit()
+    conn.close()
+
+def copy2clip(dbName, value):
     """Put snippets into clipboard."""
-    cmd = 'echo '+txt.strip()+'|clip'
-    return subprocess.check_call(cmd, shell=True)
-
+    pyperclip.copy(value)
 
 class Snippets(FlowLauncher):
 
+    def __init__(self, dbName):
+        self.dbName = dbName
+        super().__init__()
+ 
     def query(self, query):
         results = []
         try:
@@ -50,39 +57,66 @@ class Snippets(FlowLauncher):
                 if ':' in query.strip():
                     key, value = query.strip().split(':', 1)
                     results.append({
-                        "Title": "Save Snippets",
-                        "SubTitle": "key=" + key + ", Value=" + value,
+                        "Title": "Save Code Snippet",
+                        "SubTitle": "Key=" + key + ", Value=" + value,
                         "IcoPath": "assets/snippets.png", 
-                        "ContextData": "ctxData",
+                        "ContextData": [key, value],
                         "JsonRPCAction": {"method": "save", "parameters": [key.strip(), value.strip()], }})
                 else:
-                    value = getValue(query.strip())
+                    value = getValue(self.dbName, query.strip())
                     if len(value) != 0:
-                        results.append({ 
-                            "Title": "Saved⭐ " + query.strip(),
-                            "SubTitle": "Copy snippets value into clipboard",
+                        results.append({  
+                            "Title": "⭐ " + query.strip(),
+                            "SubTitle": "[Snippet] Copy to clipboard",
                             "IcoPath": "assets/snippets.png",
-                            "ContextData": "ctxData",
+                            "ContextData": [query.strip(), value],
                             "JsonRPCAction": {"method": "copy", "parameters": [value], }})
+                    else:
+                        clipboardValue = pyperclip.paste()
+                        displayValue = clipboardValue[:16] + "..." if len(clipboardValue) > 16 else ""
+                        if len(clipboardValue) != 0:
+                            results.append({
+                                "Title": "Save from clipboard",
+                                "SubTitle": "Key=" + query.strip() + ", Value=" + displayValue,
+                                "IcoPath": "assets/snippets.png",
+                                "ContextData": [query.strip(), clipboardValue],
+                                "JsonRPCAction": {"method": "save", "parameters": [query.strip(), clipboardValue], }})
 
         except:
-            # value = sys.exc_info()
-            # print('Error opening %s: %s' % (value.filename, value.strerror))
+            value = sys.exc_info()
+            print('Error opening %s: %s' % (value.filename, value.strerror))
             results.append({
                 "Title": "Code Snippets Error",
                 "SubTitle": "Please, Verify and try again",
                 "IcoPath": "assets/snippets.png", "ContextData": "ctxData"})
 
         return results
+    
+    def context_menu(self, data):
+        results = []
+        results.append({
+                        "Title": "Delete Code Snippet",
+                        "SubTitle": "Key=" + data[0] + ", Value=" + data[1],
+                        "IcoPath": "assets/snippets.png",
+                        "JsonRPCAction": {"method": "delete", "parameters": [data[0]], }})
+        results.append({
+                "Title": "Save/Update Code Snippet",
+                "SubTitle": "Key=" + data[0] + ", Value=" + data[1],
+                "IcoPath": "assets/snippets.png", 
+                "JsonRPCAction": {"method": "save", "parameters": [data[0], data[1]], }})
+        return results
 
-    def copy(self, ans):
+    def copy(self, value):
         """Copy Snippets to clipboard."""
-        FlowLauncherAPI.show_msg("Copied to clipboard", copy2clip(ans))
+        copy2clip(self.dbName, value)
 
     def save(self, key, value):
         """Save Snippets into sqlite"""
-        FlowLauncherAPI.show_msg("Saved snippets", saveValue(key.strip(), value.strip()))
+        saveValue(self.dbName, key.strip(), value.strip())
 
+    def delete(self, key):
+        """Delete Snippets from sqlite"""
+        deleteValue(self.dbName, key.strip())
 
 if __name__ == "__main__":
     Snippets()
